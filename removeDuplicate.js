5753edb53474675bf0376cf19d4f70d2f4448302
ganesh1172 <ganesh172595@gmail.com>
2024-08-13 11:11:11 +0530

    Added Remove Duplicate program

---
+++ removeDuplicate.js
@@ -0,0 +1,32 @@
+/** 
+ *  -------------------------------------------------------
+ *  Programming Question : Remove Duplicates
+ *  -------------------------------------------------------
+**/
+
+
+// Q. Write a function that takes an array of integers as input and removes any duplicates elements, returning a new array with only the unique elements. 
+
+//constraint
+//? 
+//? 
+//? 
+//? 
+
+
+function removeDuplicates(arr) {
+   // let newArr = [...new Set(arr)];
+   // return newArr
+
+   return arr.reduce((accm, curr) => {
+      if (!accm.includes(curr)) {
+         accm.push(curr)
+      }
+      return accm;
+   }, [])
+}
+
+console.log(removeDuplicates([1, 2, 3, 1, 2, 3, 2]));
+console.log(removeDuplicates([1, 2, 3, 1, 2, 4]));
+console.log(removeDuplicates([]));
+
